020e448ed3d9f3c17d2420f1a999637788d1b39b
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-04-22 19:57:14 +0200

    bonus

--- mail.js
+++ mail.js
@@ -1,22 +1,31 @@
 
 
 const listEmail = ["daniele@gmail.com", "andrea@gmail.com", "luca@gmail.com"];
+const esito = document.getElementById("esito");
 
 const button = document.getElementById("button");
 button.addEventListener("click", function () {
     event.preventDefault();
 
     const userEmail = document.getElementById("email").value;
+    let giveAccess = "";
 
 
 
 
     for (let i = 0; i < listEmail.length; i++) {
-        // console.log(i)
+
         if (userEmail == listEmail[i]) {
+            giveAccess += userEmail;
+        } else {
+
+        }
+
+        if (giveAccess != "") {
+            esito.innerHTML = "positivo";
             console.log("Può accedere " + userEmail);
         } else {
-            return;
+            esito.innerHTML = "negativo";
         }
     }
 })
